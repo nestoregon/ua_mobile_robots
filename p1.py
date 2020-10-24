6afc07a441da589bcf07159cf8f62c94ba4f4c45
Nestor <nestormorales1998@gmail.com>
2020-10-25 00:49:06 +0200

    commit

--- p1.py
+++ p1.py
@@ -6,7 +6,7 @@ Obstacle avoidance algorithm.
 Tested successfully in Gazebo 7.4 and ROS kinetic.
 
 To run:
-source ~/catkin_ws/src/devel/setup.bash # .zsh if your using a ZSH
+source ~/catkin_ws/src/devel/setup.bash # .zsh if you use ZSH
 roslaunch turtlebot_gazebo turtlebot_world.launch # starts Gazebo with turtlebot
 rosrun package_name main.py # starts this rosnode
 
